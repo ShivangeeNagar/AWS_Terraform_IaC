3148a9f202f3b7ed974ce56bdfee8eef820072ba
Shivangee Nagar <nagar.sh@northeastern.edu>
2024-02-01 10:35:04 -0500

    using provisioners to run the initial setup script

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
 variable public-key-location {}
+variable private-key-location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -115,8 +116,32 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("entry-script.sh")
+    # user_data = file("entry-script.sh")
     user_data_replace_on_change = true
+
+    # using provisioners
+
+    #connection first to the server
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_location)
+    }
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "/home/ec2-user/entry-script-on-ec2.sh"
+    }
+
+    provisioner "remote-exec" {
+        script = "entry-script.sh"
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.public_ip} > output.txt"
+    }
 
     tags = {
         Name: "${var.env_prefix}-server"
